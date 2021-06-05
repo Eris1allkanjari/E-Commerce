47766efffb9760df3dccf0f5856ba2e01c5f99d6
Eris1allkanjari <eris.allkanjari@gmail.com>
2021-06-05 15:20:34 +0200

    updated products

--- ECommerce/Models/SeedData.cs
+++ ECommerce/Models/SeedData.cs
@@ -18,93 +18,50 @@ namespace Ecommerce.Models
                 context.Products.AddRange(
                 new Product
                 {
-                    Name = "BB8",
+                    Name = "FT2232",
                     SKU = "11111",
-                    Price = .25M,
+                    Price = 30M,
                     Description = "Gumbo beet greens corn soko endive gumbo gourd.",
-                    ImgUrl = "/Assets/bb8.png",
+                    ImgUrl = "/Assets/qark1.png",
                 },
 
                 new Product
                 {
-                    Name = "Bending Unit",
+                    Name = "A4988",
                     SKU = "22222",
-                    Price = .50M,
+                    Price = 10M,
                     Description = "Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato.",
-                    ImgUrl = "/Assets/bendingunit.jpg",
+                    ImgUrl = "/Assets/qark2.png",
                 },
 
                 new Product
                 {
-                    Name = "Gundam",
+                    Name = "AS988V2",
                     SKU = "33333",
                     Price = 4.20M,
                     Description = "Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.",
-                    ImgUrl = "/Assets/gundam.jpg",
+                    ImgUrl = "/Assets/qark3.png",
                 },
 
                 new Product
                 {
-                    Name = "Android",
+                    Name = "0EV8525",
                     SKU = "44444",
                     Price = 2.50M,
                     Description = "Dandelion cucumber earthnut pea peanut soko zucchini.",
-                    ImgUrl = "/Assets/humanandroid.jpg",
+                    ImgUrl = "/Assets/qark4.png",
                 },
 
                 new Product
                 {
-                    Name = "Jukebox",
+                    Name = "DRV2522",
                     SKU = "55555",
                     Price = .75M,
                     Description = "Dandelion cucumber earthnut pea peanut soko zucchini.",
-                    ImgUrl = "/Assets/jukebox.jpg",
-                },
+                    ImgUrl = "/Assets/qark5.png",
+                }
 
-                new Product
-                {
-                    Name = "Killer Robot",
-                    SKU = "66666",
-                    Price = 3.25M,
-                    Description = "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery.",
-                    ImgUrl = "/Assets/killerrobot.jpg",
-                },
-
-                new Product
-                {
-                    Name = "Mars Rover",
-                    SKU = "77777",
-                    Price = 5.00M,
-                    Description = "Celery quandong swiss chard chicory earthnut pea potato.",
-                    ImgUrl = "/Assets/marsrover.jpg",
-                },
-
-                new Product
-                {
-                    Name = "Iron Giant",
-                    SKU = "88888",
-                    Price = 2.00M,
-                    Description = "Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. ",
-                    ImgUrl = "/Assets/miyazakirobot.jpg",
-                },
-
-                new Product
-                {
-                    Name = "Ramen Vending Robot",
-                    SKU = "99999",
-                    Price = 1.00M,
-                    Description = "Beetroot water spinach okra water chestnut ricebean pea catsear courgette summer purslane.",
-                    ImgUrl = "/Assets/ramenvending.jpg",
-                },
-
-                new Product
-                {
-                    Name = "Mechanical Bee",
-                    SKU = "1010101010",
-                    Price = .10M,
-                    Description = "Grape wattle seed kombu beetroot horseradish carrot squash brussels sprout chard.",
-                    ImgUrl = "/Assets/robotbee.jpg",
-                });
+               );
 
                 context.SaveChanges();
             }
